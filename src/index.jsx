1e959562bf4f56020b6fc0e8f35a27fe93fe084b
muhammadfurqan-code <muhammad.furqan@devscapedigital.com>
2026-06-24 17:19:23 +0500

    Generated index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,41 @@
+// Marvel Machining — generated page, sections assembled in order
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import NavbarSection from './sections/NavbarSection';
+import HeroSection from './sections/HeroSection';
+import TrustBarSection from './sections/TrustBarSection';
+import ServicesSection from './sections/ServicesSection';
+import AboutSection from './sections/AboutSection';
+import StatsSection from './sections/StatsSection';
+import CapabilitiesSection from './sections/CapabilitiesSection';
+import ProcessSection from './sections/ProcessSection';
+import TestimonialsSection from './sections/TestimonialsSection';
+import GallerySection from './sections/GallerySection';
+import CtaBannerSection from './sections/CtaBannerSection';
+import BlogPreviewSection from './sections/BlogPreviewSection';
+import ContactSection from './sections/ContactSection';
+import FooterSection from './sections/FooterSection';
+
+function Home() {
+  return (
+    <>
+      <NavbarSection />
+      <HeroSection />
+      <TrustBarSection />
+      <ServicesSection />
+      <AboutSection />
+      <StatsSection />
+      <CapabilitiesSection />
+      <ProcessSection />
+      <TestimonialsSection />
+      <GallerySection />
+      <CtaBannerSection />
+      <BlogPreviewSection />
+      <ContactSection />
+      <FooterSection />
+    </>
+  );
+}
+
+
+export default Home;
