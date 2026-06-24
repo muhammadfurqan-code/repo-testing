229b3f590f82731881c74baadae919423ec40dc7
muhammadfurqan-code <muhammad.furqan@devscapedigital.com>
2026-06-24 22:29:22 +0500

    Generated index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,39 @@
+// Matrix Plastic Products — generated page, sections assembled in order
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import NavbarSection from './sections/NavbarSection';
+import HeroSection from './sections/HeroSection';
+import ServiceTilesSection from './sections/ServiceTilesSection';
+import AboutSection from './sections/AboutSection';
+import CapabilitiesSection from './sections/CapabilitiesSection';
+import QualityCertificationsSection from './sections/QualityCertificationsSection';
+import StatsSection from './sections/StatsSection';
+import VideoTourSection from './sections/VideoTourSection';
+import WhyMatrixSection from './sections/WhyMatrixSection';
+import GallerySection from './sections/GallerySection';
+import AssociationsSection from './sections/AssociationsSection';
+import CtaBannerSection from './sections/CtaBannerSection';
+import FooterSection from './sections/FooterSection';
+
+function Home() {
+  return (
+    <>
+      <NavbarSection />
+      <HeroSection />
+      <ServiceTilesSection />
+      <AboutSection />
+      <CapabilitiesSection />
+      <QualityCertificationsSection />
+      <StatsSection />
+      <VideoTourSection />
+      <WhyMatrixSection />
+      <GallerySection />
+      <AssociationsSection />
+      <CtaBannerSection />
+      <FooterSection />
+    </>
+  );
+}
+
+
+export default Home;
